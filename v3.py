4e12307428dba291982358337f7ae5d8924f2f6b
Alex M <mawunka160688@gmail.com>
2017-09-10 19:17:20 +0300

    test comm1

--- v3.py
+++ v3.py
@@ -31,3 +31,6 @@ def repeat_all_messages(message):
 		bot.send_message(message.chat.id, 'я лучше Севы )))) ')
 if __name__ == '__main__':
 	bot.polling(none_stop=True)
+	
+#for i in y:
+#	if m.startswith(i):
