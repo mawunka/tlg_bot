b32418a6b4cb3e30a2565947f2e4bd9e97998581
Alex M <mawunka160688@gmail.com>
2017-09-10 19:09:54 +0300

    1st ver

---
+++ v3.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+import telebot, datetime, random
+token = '396281526:AAEJe7koxfwuILQcvZpRNdCALlRO5FrfYrg'
+GROUP_ID = -226450723 #test
+#GROUP_ID = -220874436 #real-group
+bot = telebot.TeleBot(token)
+hello = ['прив', 'хелло', 'здравствуйте', 'добрый вечер', 'добрый день', 'приветь', 'hello', 'hi', 'good morning', 'hey', 'ayo', 'эйо', 'zdorov', 'даров', 'дороу', 'дратути']
+hour = datetime.datetime.now().hour
+a,b,c = [18,19,20,21,22,23,24,0,1,2,3,4], [5,6,7,8,9,10,11], [12,13,14,15,16,17]
+if hour in a:
+	bot.send_message(GROUP_ID, 'Добрый вечер, господа!')
+elif hour in b:
+	bot.send_message(GROUP_ID, 'Доброе утро, господа!')
+else:
+	bot.send_message(GROUP_ID, 'Добрый день, господа!')
+@bot.message_handler(commands=['help'])
+def hlp(message):
+	bot.send_message(message.chat.id, ''' Я еще мал и глуп (( пока могу отвечать только на слова:
+	'privet', 'hello', 'hi', 'test', 'тест', 'Сева' ''')
+
+@bot.message_handler(func=lambda message: message.chat.id == GROUP_ID)
+def repeat_all_messages(message):
+	prvt = ['privet', 'hello', 'hi']
+	tst = ['test', 'тест']
+	if (message.text in prvt):
+		bot.send_message(message.chat.id, 'zdarova pidr')
+	elif (message.text in tst):
+		bot.send_message(message.chat.id, 'pass')
+	elif (message.text == 'Сева'):
+		bot.send_message(message.chat.id, 'я лучше Севы )))) ')
+if __name__ == '__main__':
+	bot.polling(none_stop=True)
