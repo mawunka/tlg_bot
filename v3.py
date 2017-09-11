0fcea6640fa5e147698c38ae08f5805b91eb40f4
Alex M_wrk <mawunka160688@gmail.com>
2017-09-11 10:48:54 +0300

    update from work1

--- v3.py
+++ v3.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 import telebot, datetime, random
 token = '396281526:AAEJe7koxfwuILQcvZpRNdCALlRO5FrfYrg'
-GROUP_ID = -226450723 #test
-#GROUP_ID = -220874436 #real-group
+#GROUP_ID = -226450723 #test
+GROUP_ID = -1001134134257 #real-group
 #test
 bot = telebot.TeleBot(token)
 hello = ['прив', 'хелло', 'здравствуйте', 'добрый вечер', 'добрый день', 'приветь', 'hello', 'hi', 'good morning', 'hey', 'ayo', 'эйо', 'zdorov', 'даров', 'дороу', 'дратути']
+hello_answ = ['И тебе привет', 'даров', 'хелоу', 'драсть', 'здарова пидр))000', 'Доброго времени суток!', 'Oi', 'Hi :)', 'hello']
 hour = datetime.datetime.now().hour
 a,b,c = [18,19,20,21,22,23,24,0,1,2,3,4], [5,6,7,8,9,10,11], [12,13,14,15,16,17]
 if hour in a:
@@ -23,12 +24,15 @@ def hlp(message):
 def repeat_all_messages(message):
 	prvt = ['privet', 'hello', 'hi']
 	tst = ['test', 'тест']
-	if (message.text in prvt):
-		bot.send_message(message.chat.id, 'zdarova pidr')
-	elif (message.text in tst):
-		bot.send_message(message.chat.id, 'pass')
-	elif (message.text == 'Сева'):
-		bot.send_message(message.chat.id, 'я лучше Севы )))) ')
+	#if (message.text in prvt):
+	#	bot.send_message(message.chat.id, 'zdarova pidr')
+	for answ in hello:
+		if message.text.lower().startswith(answ):
+			bot.send_message(message.chat.id, random.choice(hello_answ))
+	#elif (message.text in tst):
+	#	bot.send_message(message.chat.id, 'pass')
+	#elif (message.text == 'Сева'):
+	#	bot.send_message(message.chat.id, 'я лучше Севы )))) ')
 if __name__ == '__main__':
 	bot.polling(none_stop=True)
 	
